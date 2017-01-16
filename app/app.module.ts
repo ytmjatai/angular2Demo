67c443ceba3d1ef87ccc5db394e141ebf68f92a2
jatai <565735954@qq.com>
2017-01-16 09:45:48 +0800

    添加 bootstrap4,ng-bootstrap依赖

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
